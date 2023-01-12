a770569362e156490d942b16c02f82e5d0ad0fe4
zli06160 <122523068+zli06160@users.noreply.github.com>
2023-01-12 15:13:06 +0100

    First Version

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="TestZLI",
-    version="1.0",
+    version="1.1",
     packages=find_packages(),
 )
--- tests/test_uniswap_v2_price_calculator.py
+++ tests/test_uniswap_v2_price_calculator.py
@@ -5,14 +5,25 @@ from testZli.uniswap_v2_price_calculator import UniswapV2PriceCalculator
 
 class MyTestCase(unittest.TestCase):
     """
-    Here are the necessary units tests for the  UniswapV2 Price Calculator.
+    Here are the necessary units tests for the  UniSwapV2 Price Calculator.
     They should cover all types of inputs params, and be checked with manually calculated reference results.
     """
     def test_with_ones(self):
         calculator = UniswapV2PriceCalculator()
-        result = calculator.calculatePrice(1, 1, 1, 1)
+        result = calculator.calculate_price(1, 1, 1, 1)
         self.assertEqual(1, result)  # add assertion here
+
+    def test_with_twos(self):
+        calculator = UniswapV2PriceCalculator()
+        result = calculator.calculate_price(2.0, 2, 2, 2)
+        self.assertEqual(1, result)  # add assertion here
+
+    def test_with_randoms(self):
+        calculator = UniswapV2PriceCalculator()
+        result = calculator.calculate_price(2.0, 3.5, 8, 10)
+        self.assertEqual(0.3055555555555556, result)  # add assertion here
 
 
 if __name__ == '__main__':
     unittest.main()
+
